b8065a3b4ea662749bbb2839eb3efb693ca7d3c0
Lewis Cheung <lewislunsj@gmail.com>
2025-02-12 16:54:02 -0500

    fix: moved event to selectable

--- Assets/Scripts/GameBoard/SelectionManager.cs
+++ Assets/Scripts/GameBoard/SelectionManager.cs
@@ -1,59 +1,34 @@
 using UnityEngine;
-using System.Collections.Generic;
-using UnityEngine.Events;
-using UnityEngine.EventSystems;
 
 public class SelectionManager : MonoBehaviour {
 
     [Header("Info")]
-    [field:SerializeField, ReadOnly] public GameObject SelectedObject { get; private set; }
+    [field:SerializeField, ReadOnly] public Selectable SelectedItem { get; private set; }
 
     public static SelectionManager Instance { get; private set; }
 
-    protected Dictionary<GameObject, UnityEvent> OnSelectByObj { get; private set; } = new();
-    protected Dictionary<GameObject, UnityEvent> OnDeselectByObj { get; private set; } = new();
-
-    public bool ToggleSelect(GameObject obj) {
-        if (SelectedObject == obj) {
+    public bool ToggleSelect(Selectable selectable) {
+        if (SelectedItem == selectable) {
             Deselect();
             return false;
         } else {
-            return Select(obj);
+            Select(selectable);
+            return true;
         }
     }
 
-    public bool Select(GameObject obj) {
+    public void Select(Selectable selectable) {
         Deselect();
-        SelectedObject = obj;
-        if (OnSelectByObj.ContainsKey(SelectedObject)) {
-            OnSelectByObj[SelectedObject].Invoke();
-        }
-        return true;
+        SelectedItem = selectable;
+        selectable.OnSelected.Invoke();
     }
 
-    public bool Deselect() {
-        if (SelectedObject == null) {
-            return false;
+    public void Deselect() {
+        if (SelectedItem == null) {
+            return;
         }
-        if (OnDeselectByObj.ContainsKey(SelectedObject)) {
-            OnDeselectByObj[SelectedObject].Invoke();
-        }
-        
-        return true;
-    }
-
-    public void RegisterOnSelect(GameObject obj, UnityAction action) {
-        if (!OnSelectByObj.ContainsKey(obj)) {
-            OnSelectByObj[obj] = new UnityEvent();
-        }
-        OnSelectByObj[obj].AddListener(action);
-    }
-
-    public void RegisterOnDeselect(GameObject obj, UnityAction action) {
-        if (!OnDeselectByObj.ContainsKey(obj)) {
-            OnDeselectByObj[obj] = new UnityEvent();
-        }
-        OnDeselectByObj[obj].AddListener(action);
+        SelectedItem.OnDeselected.Invoke();
+        SelectedItem = null;
     }
 
     protected void Awake() {
--- Assets/Scripts/General/Selectable.cs
+++ Assets/Scripts/General/Selectable.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class Selectable: MonoBehaviour, IPointerClickHandler {
+
+    [Header("Events")]
+    public UnityEvent OnSelected = new();  // invoked by SelectionManager
+    public UnityEvent OnDeselected = new();  // invoked by SelectionManager
+
     public void OnPointerClick(PointerEventData eventData) {
-        SelectionManager.Instance.ToggleSelect(gameObject);
+        SelectionManager.Instance.ToggleSelect(this);
     }
 }
