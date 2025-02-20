2d1b4dd2b300f1844dada8d48a79bd42cbe4e07e
Lewis Cheung <lewislunsj@gmail.com>
2025-02-20 17:00:25 -0500

    fix: remove effects when wave is not active

--- Assets/Scripts/Buildings/Functions/BuildingEffect.cs
+++ Assets/Scripts/Buildings/Functions/BuildingEffect.cs
@@ -11,6 +11,12 @@ public abstract class BuildingEffect : MonoBehaviour {
     [field: SerializeField, ReadOnly] public Building Building { get; private set; }
 
     public virtual void Apply(Building building) {
+        if (!LevelManager.Instance.IsWaveActive) {
+            Debug.LogError("Cannot apply effect when wave is not active");
+            Destroy(gameObject);
+            return;
+        }
+
         if (building.EffectByType.ContainsKey(GetType())) {
             building.EffectByType[GetType()].Reapply(this);
             Destroy(gameObject);
@@ -20,6 +26,7 @@ public abstract class BuildingEffect : MonoBehaviour {
         Building = building;
         building.EffectByType[GetType()] = this;
         OnApply.Invoke();
+        LevelManager.Instance.OnWaveEnd.AddListener(Remove);
     }
 
     public abstract void Reapply(BuildingEffect otherEffect);
@@ -27,6 +34,7 @@ public abstract class BuildingEffect : MonoBehaviour {
     public virtual void Remove() {
         OnRemove.Invoke();
         Building.EffectByType.Remove(GetType());
+        LevelManager.Instance.OnWaveEnd.RemoveListener(Remove);
         Destroy(gameObject);
     }
 }
